0f3b0982006a85183f4244ce7ea633e6323cf909
TimothySjiang <shjiang@ucdavis.edu>
2019-09-03 16:58:48 -0700

    Initial commit

---
+++ app/src/main/java/com/shan/self/PostJournalActivity.java
@@ -0,0 +1,141 @@
+package com.shan.self;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Objects;
+
+import util.JournalApi;
+
+public class PostJournalActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int GALLERY_CODE = 1;
+    private static final String TAG = "PostJournalActivity";
+    private Button saveButton;
+    private ProgressBar progressBar;
+    private ImageView addPhotoButton;
+    private EditText titleEditText;
+    private EditText thoughtsEditText;
+    private TextView currentUserTextView;
+    private ImageView imageView;
+
+    private String currentUserId;
+    private String currentUserName;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
+    //Connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+
+    private CollectionReference collectionReference = db.collection("Journal");
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_journal);
+
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.post_progressBar);
+        titleEditText = findViewById(R.id.post_title_et);
+        thoughtsEditText = findViewById(R.id.post_description_et);
+        currentUserTextView = findViewById(R.id.post_username_textview);
+
+        imageView = findViewById(R.id.post_imageView);
+        saveButton = findViewById(R.id.post_save_journal_button);
+        saveButton.setOnClickListener(this);
+        addPhotoButton = findViewById(R.id.postCameraButton);
+        addPhotoButton.setOnClickListener(this);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+        if (JournalApi.getInstance() != null) {
+            currentUserId = JournalApi.getInstance().getUserId();
+            currentUserName = JournalApi.getInstance().getUsername();
+
+            currentUserTextView.setText(currentUserName);
+        }
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                } else {
+
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.post_save_journal_button:
+                //saveJournal
+                //saveJournal();
+                break;
+            case R.id.postCameraButton:
+                //get image from gallery/phone
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                imageUri = data.getData(); // we have the actual path to the image
+                imageView.setImageURI(imageUri);//show image
+
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+}
